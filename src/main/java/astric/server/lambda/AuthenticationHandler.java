e479096577fcfeeced84a7ce4ba00a8e99487d2a
Jared Hasson <jhasson@simplifile.com>
2020-04-10 11:41:23 -0600

    implemented log out

--- src/main/java/astric/server/lambda/AuthenticationHandler.java
+++ src/main/java/astric/server/lambda/AuthenticationHandler.java
@@ -4,8 +4,6 @@ import astric.server.dao.AuthDAOImpl;
 
 public class AuthenticationHandler {
 
-    public static AuthDAOImpl authDAO;
-
     public static void authenticateRequest(String authToken) throws RuntimeException {
         if (!new AuthDAOImpl().sessionIsValid(authToken)) {
             throw new RuntimeException("[RequestError] Invalid auth token, your session may have expired. Please login again.");
